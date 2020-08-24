fc177b123b1e5716c2e1927945de65161f5e8c0d
wrmsr <timwilloney@gmail.com>
2020-08-24 02:41:25 -0700

    --

--- iceworm/utils/cron.py
+++ iceworm/utils/cron.py
@@ -1,8 +1,13 @@
+"""
+https://en.wikipedia.org/wiki/Cron
+"""
 import typing as ta
 
 from omnibus import check
 from omnibus import collections as ocol
 from omnibus import dataclasses as dc
+
+from .utils import seq
 
 
 T = ta.TypeVar('T')
@@ -45,29 +50,55 @@ WEEKDAYS = _enum([
 ])
 
 
+class Range(dc.Pure):
+    min: int = dc.field(check=int.__instancecheck__)
+    max: int = dc.field(check=int.__instancecheck__)
+
+    def __post_init__(self) -> None:
+        check.arg(self.min <= self.max)
+
+    def __str__(self) -> str:
+        return f'[{self.min}, {self.max}]' if self.min != self.max else str(self.min)
+
+    def __contains__(self, i: int) -> bool:
+        return self.min <= check.isinstance(i, int) <= self.max
+
+
 class Field(dc.Pure):
     name: str
     idx: int
-    min: int
-    max: int
+    rng: Range
     enum: ta.Optional[Enum] = None
 
 
 FIELDS = [
-    Field('minute', 0, 0, 59),
-    Field('hour', 1, 0, 23),
-    Field('day', 2, 1, 31),
-    Field('month', 3, 1, 12, MONTHS),
-    Field('weekday', 4, 0, 6, WEEKDAYS),
+    Field('minute', 0, Range(0, 59)),
+    Field('hour', 1, Range(0, 23)),
+    Field('day', 2, Range(1, 31)),
+    Field('month', 3, Range(1, 12), MONTHS),
+    Field('weekday', 4, Range(0, 6), WEEKDAYS),
 ]
 
 
+class Item(dc.Pure):
+    rngs: ta.Sequence[Range] = dc.field(coerce=seq, check=lambda l: l and all(isinstance(o, Range) for o in l))
+
+    def __str__(self) -> str:
+        return f"{','.join(self.rngs)}" if len(self.rngs) > 1 else str(check.single(self.rngs))
+
+    def __iter__(self) -> ta.Iterator:
+        return iter(self.rngs)
+
+
 class Entry(dc.Pure):
-    minute: ta.Optional[int] = dc.field(None, kwonly=True)
-    hour: ta.Optional[int] = dc.field(None, kwonly=True)
-    day: ta.Optional[int] = dc.field(None, kwonly=True)
-    month: ta.Optional[int] = dc.field(None, kwonly=True)
-    weekday: ta.Optional[int] = dc.field(None, kwonly=True)
+    minute: ta.Optional[Item] = dc.field(None, kwonly=True)
+    hour: ta.Optional[Item] = dc.field(None, kwonly=True)
+    day: ta.Optional[Item] = dc.field(None, kwonly=True)
+    month: ta.Optional[Item] = dc.field(None, kwonly=True)
+    weekday: ta.Optional[Item] = dc.field(None, kwonly=True)
+
+    def __str__(self):
+        return ' '.join(str(i) if i is not None else '*' for f in FIELDS for i in [getattr(self, f.name)])
 
 
 def parse(s: str) -> Entry:
@@ -78,7 +109,8 @@ def parse(s: str) -> Entry:
         if p == '*':
             continue
         n = int(p)
-        kw[f.name] = n
+        check.arg(n in f.rng)
+        kw[f.name] = Item([Range(n, n)])
     return Entry(**kw)
 
 
