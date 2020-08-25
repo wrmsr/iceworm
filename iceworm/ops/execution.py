b5c96944483f71872f43a6071fb3105cb3efd202
wrmsr <timwilloney@gmail.com>
2020-08-25 15:34:32 -0700

    --

--- iceworm/ops/execution.py
+++ iceworm/ops/execution.py
@@ -11,52 +11,56 @@ from omnibus import dataclasses as dc
 from omnibus import lang
 import sqlalchemy as sa
 
+from . import ops
 from .. import alchemy as alch
 from .. import sql
 from ..types import QualifiedName
 from .connectors import ConnectionSet
-from .ops import AtomicCreateTableAs
-from .ops import InsertIntoSelect
-from .ops import CreateTable
-from .ops import DropTable
-from .ops import Op
-from .ops import Transaction
 from .sql import SqlConnection
 from .utils import parse_simple_select_table
 
 
 OpT = ta.TypeVar('OpT', bound=Op)
+OpGen = ta.Generator[ops.Op, None, None]
 
 
 class Executor(lang.Abstract, ta.Generic[OpT]):
+
+    @abc.abstractmethod
+    def execute(self, op: OpT) -> ta.Optional[OpGen]:
+        raise NotImplementedError
+
+
+class ListExecutor(Executor[ops.List]):
+
+    def execute(self, op: ops.List) -> OpGen:
+        yield from op.ops
+
+
+class ConnsExecutor(Executor[OpT], lang.Abstract):
 
     def __init__(self, conns: ConnectionSet) -> None:
         super().__init__()
 
         self._conns = check.isinstance(conns, ConnectionSet)
 
-    @abc.abstractmethod
-    def execute(self, op: OpT) -> ta.Union[None, ta.Generator[Op, None, None]]:
-        raise NotImplementedError
 
+class TransactionExecutor(ConnsExecutor[ops.Transaction]):
 
-class TransactionExecutor(Executor[Transaction]):
-
-    def execute(self, op: Transaction) -> ta.Generator[Op, None, None]:
+    def execute(self, op: ops.Transaction) -> OpGen:
         sa_conn = check.isinstance(self._conns[check.single(op.conns)], SqlConnection).sa_conn
         with sa_conn.begin() as txn:
             try:
-                for child in op.children:
-                    yield child
+                yield op.op
                 txn.commit()
             except Exception:
                 txn.rollback()
                 raise
 
 
-class DropTableExecutor(Executor[DropTable]):
+class DropTableExecutor(ConnsExecutor[ops.DropTable]):
 
-    def execute(self, op: DropTable) -> None:
+    def execute(self, op: ops.DropTable) -> None:
         sa_conn = check.isinstance(self._conns[op.name[0]], SqlConnection).sa_conn
         sa_conn.execute(
             sql.DropTableIfExists(
@@ -64,17 +68,17 @@ class DropTableExecutor(Executor[DropTable]):
                     QualifiedName(op.name[1:]))))
 
 
-class CreateTableExecutor(Executor[CreateTable]):
+class CreateTableExecutor(ConnsExecutor[ops.CreateTable]):
 
-    def execute(self, op: CreateTable) -> None:
+    def execute(self, op: ops.CreateTable) -> None:
         sa_conn = check.isinstance(self._conns[op.table.name[0]], SqlConnection).sa_conn
         table = alch.FromInternal(sa.MetaData())(dc.replace(op.table, name=op.table.name[1:]))
         table.create(sa_conn)
 
 
-class AtomicCreateTableAsExecutor(Executor[AtomicCreateTableAs]):
+class AtomicCreateTableAsExecutor(ConnsExecutor[ops.AtomicCreateTableAs]):
 
-    def execute(self, op: AtomicCreateTableAs) -> None:
+    def execute(self, op: ops.AtomicCreateTableAs) -> None:
         sa_conn = check.isinstance(self._conns[op.name[0]], SqlConnection).sa_conn
         sa_conn.execute(
             sql.CreateTableAs(
@@ -83,9 +87,9 @@ class AtomicCreateTableAsExecutor(Executor[AtomicCreateTableAs]):
                 sa.text(op.query)))
 
 
-class InsertIntoSelectExecutor(Executor[InsertIntoSelect]):
+class InsertIntoSelectExecutor(ConnsExecutor[ops.InsertIntoSelect]):
 
-    def execute(self, op: InsertIntoSelect) -> None:
+    def execute(self, op: ops.InsertIntoSelect) -> None:
         dst_conn = self._conns[op.dst[0]]
 
         src_name = parse_simple_select_table(op.query, star=True)
--- iceworm/ops/ops.py
+++ iceworm/ops/ops.py
@@ -1,8 +1,8 @@
 """
 TODO:
- - Sequence? Block?
-  - ** distinction between ordered (opaque) blocks and unordered sets? **
-   - is *anything* opaque?
+ - ** blurs line between task and target **
+ - ** distinction between ordered (opaque) blocks and unordered sets? **
+  - is *anything* opaque?
  - 'merge'? refresh?
  - from alerts:
   - conditional? takes query?
@@ -30,9 +30,17 @@ OpGen = ta.Generator['Op', None, None]
 OpMapper = ta.Callable[['Op'], 'Op']
 
 
+class List(Op):
+    ops: ta.Sequence[Op] = dc.field(coerce=seq, check=lambda l: all(isinstance(o, Op) for o in l))
+
+
+class Set(Op):
+    ops: ta.Sequence[Op] = dc.field(coerce=seq, check=lambda l: all(isinstance(o, Op) for o in l))
+
+
 class Transaction(Op):
     conns: ta.AbstractSet[str] = dc.field(coerce=abs_set, check=lambda l: all(isinstance(o, str) for o in l))
-    children: ta.Sequence[Op] = dc.field(coerce=seq, check=lambda l: all(isinstance(o, Op) for o in l))
+    op: Op = dc.field(coerce=seq, check=lambda o: isinstance(o, Op))
 
 
 class Atomic(Op, abstract=True):
