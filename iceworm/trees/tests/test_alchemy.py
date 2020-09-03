8f408d37e8b3ba82fe100b46bea0047bd85a9179
wrmsr <timwilloney@gmail.com>
2020-09-03 15:09:17 -0700

    --

--- iceworm/trees/tests/test_alchemy.py
+++ iceworm/trees/tests/test_alchemy.py
@@ -3,9 +3,24 @@ import sqlalchemy.sql.elements
 
 from .. import alchemy as alch
 from .. import nodes as no
+from .. import parsing as par
 
 
 def test_alchemy():
     node = no.Integer(420)
     xlat = alch.transmute(node)
     assert isinstance(xlat, sa.sql.elements.BindParameter)
+
+    for query in [
+        'select 1',
+        'select * from barf',
+        'select * from a join b',
+        'select * from a join b where 1',
+        'select * from a join b where a.b',
+        'select * from a join b where a.b and 2',
+        'select * from a join b where a.b and 2 > 3',
+    ]:
+        print(query)
+        stmt = alch.transmute(par.parse_statement(query + ';'))
+        print(stmt)
+        print()
