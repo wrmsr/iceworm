6e4d48717c5d1f44d16629fbd41c40f409403bd5
wrmsr <timwilloney@gmail.com>
2020-09-03 13:25:49 -0700

    --

--- iceworm/engine/state.py
+++ iceworm/engine/state.py
@@ -22,36 +22,119 @@ tables:
    - historical invalidations? pending? handled? cleared at max interval according to scheds?
 """
 import abc
+import typing as ta
 
 from omnibus import check
 from omnibus import lang
 
-
-class State(lang.Abstract):
-
-    @abc.abstractmethod
-    def get_object(self):
-        raise NotImplementedError
+from ..utils import unique_dict
 
 
-class HeapState(State):
+StrMap = ta.Mapping[str, ta.Any]
+Key = StrMap
+KeyTup = ta.Sequence[ta.Any]
+Obj = ta.Any
+ObjCls = ta.Type[Obj]
 
-    def get_object(self):
-        raise NotImplementedError
 
+class ObjMapper:
 
-class SqlState(State):
-
-    class Backend(lang.Abstract):
-        pass
-
-    class PostgresBackend(Backend):
-        pass
-
-    def __init__(self, backend: Backend) -> None:
+    def __init__(self, cls: ObjCls, key_fields: ta.Sequence[str]) -> None:
         super().__init__()
 
-        self._backend = check.isinstance(backend, SqlState.Backend)
+        self._cls = check.isinstance(cls, type)
+        self._key_fields = [check.isinstance(f, str) for f in check.not_isinstance(key_fields, str)]
+        self._key_fields_set = set(self._key_fields)
 
-    def get_object(self):
+    @property
+    def cls(self) -> ObjCls:
+        return self._cls
+
+    @property
+    def key_fields(self) -> ta.Sequence[str]:
+        return self._key_fields
+
+    def obj_to_key_tup(self, obj: Obj) -> KeyTup:
+        if type(obj) is not self._cls:
+            raise TypeError(obj, self._cls)
+        return tuple(getattr(obj, f) for f in self._key_fields)
+
+    def obj_to_key(self, obj: Obj) -> Key:
+        if type(obj) is not self._cls:
+            raise TypeError(obj, self._cls)
+        return {f: getattr(obj, f) for f in self._key_fields}
+
+    def key_to_key_tup(self, key: Key) -> KeyTup:
+        if set(key) != self._key_fields_set:
+            raise KeyError(key, self._key_fields_set)
+        return tuple(key[f] for f in self._key_fields)
+
+
+class ObjStore(lang.Abstract):
+
+    @abc.abstractmethod
+    def key(self, obj: Obj) -> Key:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, cls: ObjCls, key: Key) -> Obj:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def put(self, obj: ObjCls) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def delete(self, cls: ObjCls, key: Key) -> None:
+        raise NotImplementedError
+
+
+class HeapObjStore(ObjStore):
+
+    def __init__(self, mappers: ta.Iterable[ObjMapper]) -> None:
+        super().__init__()
+
+        self._mappers_by_cls = unique_dict((sm.cls, sm) for sm in mappers for _ in [check.isinstance(sm, ObjMapper)])
+        self._objs_by_key_tup_by_cls: ta.Dict[ta.Dict[KeyTup, Obj]] = {}
+
+    def key(self, obj: Obj) -> Key:
+        return self._mappers_by_cls[type(obj)].obj_to_key(obj)
+
+    def get(self, cls: ObjCls, key: Key) -> Obj:
+        kt = self._mappers_by_cls[cls].key_to_key_tup(key)
+        return check.isinstance(self._objs_by_key_tup_by_cls[cls][kt], cls)
+
+    def put(self, obj: ObjCls) -> None:
+        cls = type(obj)
+        kt = self._mappers_by_cls[cls].obj_to_key_tup(obj)
+        self._objs_by_key_tup_by_cls.setdefault(cls, {})[kt] = obj
+
+    def delete(self, cls: ObjCls, key: Key) -> None:
+        kt = self._mappers_by_cls[cls].key_to_key_tup(key)
+        del self._objs_by_key_tup_by_cls[cls][kt]
+
+
+class SqlObjStore(ObjStore):
+
+    class Adapter(lang.Abstract):
+        pass
+
+    class PostgresAdapter(Adapter):
+        pass
+
+    def __init__(self, adapter: Adapter) -> None:
+        super().__init__()
+
+        self._adapter = check.isinstance(adapter, SqlObjStore.Adapter)
+
+    def key(self, obj: Obj) -> Key:
+        raise NotImplementedError
+
+    def get(self, cls: ObjCls, key: Key) -> Obj:
+        raise NotImplementedError
+
+    def put(self, obj: ObjCls) -> None:
+        raise NotImplementedError
+
+    def delete(self, cls: ObjCls, key: Key) -> None:
         raise NotImplementedError
--- iceworm/engine/tests/test_state.py
+++ iceworm/engine/tests/test_state.py
@@ -2,18 +2,40 @@
 TODO:
  -
 """
+import dataclasses as dc
 import os.path
 
 from omnibus import lang
 import pytest
 import sqlalchemy as sa
 
+from .. import state as st
 from ...tests.helpers import clean_pg
 from ...tests.helpers import pg_url  # noqa
 
 
+@dc.dataclass(frozen=True)
+class WorldState:
+    id: int
+    a: str
+    b: str
+
+
+def test_state():
+    store = st.HeapObjStore([
+        st.ObjMapper(WorldState, ['id']),
+    ])
+    wos = [
+        WorldState(0, 'aa', 'bb'),
+        WorldState(1, 'cc', 'dd'),
+    ]
+    store.put(wos[0])
+    store.put(wos[1])
+    assert store.get(WorldState, store.key(wos[0])) == wos[0]
+
+
 @pytest.mark.xfail()
-def test_state(pg_url):  # noqa
+def test_state_script(pg_url):  # noqa
     with open(os.path.join(os.path.dirname(__file__), 'state.sql'), 'r') as f:
         buf = f.read()
 
