883fdc3a9d09cbc7f4c1aa58a39763d3bc71eda7
wrmsr <timwilloney@gmail.com>
2020-09-07 21:04:21 -0700

    --

---
+++ iceworm/tests/test_jvm.py
@@ -0,0 +1,15 @@
+import os
+
+import pytest
+
+
+@pytest.mark.xfail()
+def test_jpype():
+    os.environ['JAVA_HOME'] = '/Library/Java/JavaVirtualMachines/jdk1.8.0_261.jdk/Contents/Home'
+    import jpype
+    jpype.startJVM(classpath=['lib/*', 'classes'], convertStrings=False)
+    try:
+        from jpype import java
+        print(java.lang.System.getProperty('java.vendor'))
+    finally:
+        jpype.shutdownJVM()
