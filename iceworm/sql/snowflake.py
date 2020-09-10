e8bd2704112e9e69c7e02d8650ec724be2cc6435
wrmsr <timwilloney@gmail.com>
2020-09-09 18:01:42 -0700

    --

--- iceworm/sql/snowflake.py
+++ iceworm/sql/snowflake.py
@@ -132,6 +132,37 @@ $$ ;
 
 
 @lang.cached_nullary
+def patch_ssl_wrapper():
+    from snowflake.connector import ssl_wrap_socket
+
+    import threading
+    local = threading.local()
+
+    def ssl_wrap_socket_with_ocsp(*args, **kwargs):
+        local.depth = getattr(local, 'depth', 0) + 1
+        if local.depth >= 3:
+            raise RuntimeError('snowflake ssl monkeypatch will infinitely recurse')
+        try:
+            return _ssl_wrap_socket_with_ocsp(*args, **kwargs)
+        finally:
+            local.depth -= 1
+
+    _ssl_wrap_socket_with_ocsp = ssl_wrap_socket.ssl_wrap_socket_with_ocsp
+    ssl_wrap_socket.ssl_wrap_socket_with_ocsp = ssl_wrap_socket_with_ocsp
+
+    def inject_into_urllib3():
+        ssl_wrap_socket.connection_.ssl_wrap_socket = ssl_wrap_socket_with_ocsp
+
+    ssl_wrap_socket.inject_into_urllib3 = inject_into_urllib3
+
+    if ssl_wrap_socket.connection_.ssl_wrap_socket == _ssl_wrap_socket_with_ocsp:
+        ssl_wrap_socket.connection_.ssl_wrap_socket = ssl_wrap_socket_with_ocsp
+
+
+patch_ssl_wrapper()
+
+
+@lang.cached_nullary
 def get_config() -> ta.Mapping[str, str]:
     config_file_path = os.environ['ICEWORM_SNOWFLAKE_CONFIG_PATH']
     with open(os.path.expanduser(config_file_path), 'r') as f:
