af3001d3c0f427edf174890050d9426e02782e51
wrmsr <timwilloney@gmail.com>
2020-09-21 03:22:23 -0700

    --

--- iceworm/tests/test_spark.py
+++ iceworm/tests/test_spark.py
@@ -27,12 +27,18 @@ SPARK_HOME=../.venv/lib/python3.7/site-packages/pyspark \
 stop org.apache.spark.sql.hive.thriftserver.HiveThriftServer2 1 \
 )
 """
+import importlib.util
 import operator
+import os.path
 import tempfile
 import textwrap
+import time
 import typing as ta
 
+from omnibus import check
 from omnibus import lang
+from omnibus import lifecycles as lc
+from omnibus import os as oos
 from omnibus import properties
 from omnibus.dev.testing.helpers import skip_if_cant_import
 from omnibus.spark import local as sl
@@ -44,6 +50,58 @@ if ta.TYPE_CHECKING:
     import pyspark as ps
 else:
     ps = lang.proxy_import('pyspark')
+
+from . import harness as har
+from ..utils import timebomb
+from .plugins import switches
+
+
+@har.bind(har.Session)
+class SparkManager(lc.ContextManageableLifecycle):
+
+    def __init__(self, request: har.FixtureRequest) -> None:
+        super().__init__()
+
+        self._request = check.isinstance(request, har.FixtureRequest)
+
+    @properties.stateful_cached
+    @property
+    def thrift_url(self) -> str:
+        switches.skip_if_disabled(self._request, 'spark')
+
+        from thrift.transport.TTransport import TTransportException
+
+        spark_home = os.path.abspath(os.path.dirname(importlib.util.find_spec('pyspark').origin))
+        exe = os.path.join(spark_home, 'sbin/spark-daemon.sh')
+        check.state(os.path.isfile(exe))
+
+        cls = 'org.apache.spark.sql.hive.thriftserver.HiveThriftServer2'
+        cwd = self._lifecycle_exit_stack.enter_context(oos.tmp_dir())
+        env = {
+            'JAVA_HOME': sl.LocalLauncher('exit(1)').java_home,
+            'SPARK_HOME': spark_home,
+        }
+
+        start_cmd = f'{exe} submit {cls} 1 --name "Thrift JDBC/ODBC Server"'
+        stop_cmd = f'{exe} stop {cls} 1'
+
+        self._lifecycle_exit_stack.enter_context(lang.defer(lambda: os.system(stop_cmd)))
+        self._lifecycle_exit_stack.enter_context(timebomb.start(stop_cmd, cwd=cwd, env=env, timeout=30))
+        os.system(start_cmd)
+
+        url = 'hive://localhost:10000/default'
+        while True:
+            try:
+                with lang.disposing(sa.create_engine(url)) as engine:
+                    with engine.connect() as conn:
+                        rows = list(conn.execute(sa.select([1])))
+                check.state(check.single(check.single(rows)) == 1)
+                break
+            except TTransportException as e:  # noqa
+                pass
+            time.sleep(1)
+
+        return url
 
 
 class Driver:
@@ -97,12 +155,11 @@ def test_local_launcher():
 
 
 @pytest.mark.spark
-@pytest.mark.xfail
-def test_sql():
+def test_sql(harness: har.Harness):
     # conn = hive.Connection(host='localhost', port=10000)
     # cur = conn.cursor()
     # print(list(cur.execute('select 1')))
 
-    engine = sa.create_engine('hive://localhost:10000/default')
+    engine = sa.create_engine(harness[SparkManager].thrift_url)
     with engine.connect() as conn:
         print(list(conn.execute(sa.select([1]))))
