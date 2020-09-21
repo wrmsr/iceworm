81ec6664cbf86a497580e49d6ce0c3de1d3da65a
wrmsr <timwilloney@gmail.com>
2020-09-20 23:08:47 -0700

    --

--- iceworm/utils/inject.py
+++ iceworm/utils/inject.py
@@ -1,11 +1,18 @@
+import functools
 import typing as ta
 import weakref
 
 from omnibus import check
 from omnibus import collections as ocol
 from omnibus import dataclasses as dc
+from omnibus import dynamic as dyn
 from omnibus import inject as inj
+from omnibus import lang
 from omnibus import lifecycles as lc
+from omnibus import properties
+
+
+T = ta.TypeVar('T')
 
 
 def bind_contextmanager_lifecycle(binder: inj.Binder, target: ta.Union[inj.Key, ta.Type]) -> None:
@@ -76,3 +83,50 @@ class _LifecycleRegistrar:
             self._stack[-1].dependencies.extend(st.dependencies)
 
         return instance
+
+
+class SimpleDynamicScope(inj.Scope, lang.Abstract):
+
+    class _State(ta.NamedTuple):
+        seed: ta.Mapping[inj.Key, ta.Any]
+        binds: ta.MutableMapping[inj.Binding, ta.Any]
+
+    def provide(self, binding: inj.Binding[T]) -> T:
+        vals = self._CURRENT()
+        try:
+            return vals[binding]
+        except KeyError:
+            try:
+                return vals[binding.key]
+            except KeyError:
+                val = vals[binding] = binding.provide()
+                return val
+
+    @properties.cached_class
+    def _CURRENT(cls) -> dyn.Var[_State]:  # noqa
+        return dyn.Var()
+
+    @classmethod
+    def _seed(cls, injector: inj.Injector, seed: ta.Mapping[ta.Union[type, inj.Key], ta.Any]) -> ta.Mapping[inj.Key, ta.Any]:  # noqa
+        return {inj.Key(k) if isinstance(k, type) else check.isinstance(k, inj.Key): v for k, v in seed.items()}
+
+    @properties.cached_class
+    def enter(cls):  # noqa
+        return functools.partial(_enter_simple_dynamic_scope, cls)
+
+    @classmethod
+    def _setup(cls, injector: inj.Injector, state: _State) -> None:
+        pass
+
+
+@dyn.contextmanager
+def _enter_simple_dynamic_scope(cls: ta.Type[SimpleDynamicScope], injector: inj.Injector, *args, **kwargs) -> ta.Generator[None, None, None]:  # noqa
+    check.isinstance(injector, inj.Injector)
+    state = cls._State(
+        seed=cls._seed(injector, *args, **kwargs),  # Noqa
+        binds=ocol.IdentityKeyDict(),
+    )
+    with cls._CURRENT(state):
+        with inj.Injector._CURRENT(injector):
+            cls._setup(injector)
+        yield
