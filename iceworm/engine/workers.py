653132f7489b77ba4d9b8ffacaae78f694051314
wrmsr <timwilloney@gmail.com>
2020-09-22 00:33:52 -0700

    --

--- iceworm/engine/workers.py
+++ iceworm/engine/workers.py
@@ -2,4 +2,7 @@
 TODO:
  - cancelling, preemption, worker pinning / allocation (maximize nic utilization across workers)
  - autoscaling? preemptive?
+ - usecase for remote workers beyond just security - flippability
+ - 'log' table - not text, but boxed items like exceptions, successes, stats, etc
+ - track invalidation origins - 'why is this worker doing this thing'
 """
