d38fe386fb278f0618308b52a375acc0e9c9da9c
wrmsr <timwilloney@gmail.com>
2020-09-23 00:22:17 -0700

    --

--- setup.py
+++ setup.py
@@ -159,26 +159,23 @@ class InstallCommand(setuptools.command.install.install):
                 pid_chain.append(tup)
                 cur_pid = tup[0]
 
-            """
-            29251 29243 /usr/local/Cellar/python@3.8/3.8.5/Frameworks/Python.framework/Versions/3.8/Resources/Python.app/Contents/MacOS/Python -u -c import sys, setuptools, tokenize; sys.argv[0] = '/private/var/folders/s7/z0_hyvy14txdpyh8j7j4bljr0000gn/T/pip-req    -build-9ehuvpb0/setup.py'; __file__='/private/var/folders/s7/z0_hyvy14txdpyh8j7j4bljr0000gn/T/pip-req-build-9ehuvpb0/setup.py';f=getattr(tokenize, 'open', open)(__file__);code=f.read().replace('\r\n', '\n');f.close();exec(compile(code, __file__, 'exe    c')) bdist_wheel -d /private/var/folders/s7/z0_hyvy14txdpyh8j7j4bljr0000gn/T/pip-wheel-84jjswdf
-            29243 29235 /usr/local/Cellar/python@3.8/3.8.5/Frameworks/Python.framework/Versions/3.8/Resources/Python.app/Contents/MacOS/Python -m pip install --no-dependencies /Users/spinlock/src/wrmsr/iceworm/dist/iceworm-0.0.1.dev0.zip
-            29235 88867 /usr/local/Cellar/python@3.8/3.8.5/Frameworks/Python.framework/Versions/3.8/Resources/Python.app/Contents/MacOS/Python /usr/local/Cellar/pipx/0.15.5.1/libexec/bin/pipx install --verbose --force src/wrmsr/iceworm/dist/iceworm-0.0.1.dev0.zi    p
-            88867   713 -zsh
-            713     1 tmux -2
-            29279 29267 /usr/local/Cellar/python@3.8/3.8.5/Frameworks/Python.framework/Versions/3.8/Resources/Python.app/Contents/MacOS/Python -u -c import sys, setuptools, tokenize; sys.argv[0] = '/private/var/folders/s7/z0_hyvy14txdpyh8j7j4bljr0000gn/T/pip-req    -build-jr3wqzfh/setup.py'; __file__='/private/var/folders/s7/z0_hyvy14txdpyh8j7j4bljr0000gn/T/pip-req-build-jr3wqzfh/setup.py';f=getattr(tokenize, 'open', open)(__file__);code=f.read().replace('\r\n', '\n');f.close();exec(compile(code, __file__, 'exe    c')) bdist_wheel -d /private/var/folders/s7/z0_hyvy14txdpyh8j7j4bljr0000gn/T/pip-wheel-_u9fhm5c
-            29267 29235 /usr/local/Cellar/python@3.8/3.8.5/Frameworks/Python.framework/Versions/3.8/Resources/Python.app/Contents/MacOS/Python -m pip install /Users/spinlock/src/wrmsr/iceworm/dist/iceworm-0.0.1.dev0.zip
-            29235 88867 /usr/local/Cellar/python@3.8/3.8.5/Frameworks/Python.framework/Versions/3.8/Resources/Python.app/Contents/MacOS/Python /usr/local/Cellar/pipx/0.15.5.1/libexec/bin/pipx install --verbose --force src/wrmsr/iceworm/dist/iceworm-0.0.1.dev0.zi    p
-            88867   713 -zsh
-            713     1 tmux -2
-            """
+            if len(pid_chain) >= 3:
+                p1 = pid_chain[1][1].split()
+                p2 = pid_chain[2][1].split()
 
-            with open('/tmp/asdf', 'a') as f:
-                f.write(repr(pid_chain) + '\n')
+                if (
+                        len(p1) >= 6 and
+                        p1[0].lower().endswith('/python') and
+                        p1[1:4] == ['-m', 'pip', 'install'] and
+                        p1[5] != '--no-dependencies' and
+                        len(p2) >= 3 and
+                        p2[0].lower().endswith('/python') and
+                        p2[1].lower().endswith('/pipx')
+                ):
+                    self.announce('activating pipx extras')
 
-            # self.announce('activating pipx extras')
-
-            # self.sysdeps = 1
-            # self.cyaml = 1
+                    self.sysdeps = 1
+                    self.cyaml = 1
 
     def run(self):
         if self.sysdeps:
