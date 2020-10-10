b50045e65e676bc557a4689d4118ed6c292850db
wrmsr <timwilloney@gmail.com>
2020-10-09 18:24:37 -0700

    --

--- iceworm/engine/tests/test_inject.py
+++ iceworm/engine/tests/test_inject.py
@@ -9,6 +9,7 @@ from omnibus import dataclasses as dc
 from omnibus import inject as inj
 from omnibus import lang  # noqa
 from omnibus import os as oos  # noqa
+from omnibus import properties
 from omnibus.inject.dev.pytest import harness as har
 import pytest  # noqa
 import yaml  # noqa
@@ -56,15 +57,26 @@ class UrlSecretsReplacer(els.ElementProcessor):
         ]
 
 
-# @pytest.mark.xfail
-def test_inject(harness: har.Harness):
+@pytest.yield_fixture()
+def exit_stack() -> ta.Iterator[contextlib.ExitStack]:
     with contextlib.ExitStack() as es:
-        es.enter_context(oos.tmp_chdir(os.path.dirname(__file__)))
+        yield es
 
-        secrets = sec.Secrets({'pg_url': harness[DbManager].pg_url})
 
+class TestHelper:
+
+    def __init__(self, harness: har.Harness) -> None:
+        super().__init__()
+        self._harness = harness
+
+    @properties.cached
+    @property
+    def secrets(self) -> sec.Secrets:
+        return sec.Secrets({'pg_url': self._harness[DbManager].pg_url})
+
+    def build_elements(self) -> els.ElementSet:
         binder = inj.create_binder()
-        binder.bind(sec.Secrets, to_instance=secrets)
+        binder.bind(sec.Secrets, to_instance=self.secrets)
         sql.inject.install(binder)
         einj.install(binder)
 
@@ -74,7 +86,6 @@ def test_inject(harness: har.Harness):
         els.inject.bind_elements_module(binder, _install_elements)
 
         binder.bind(inj.Key(ta.Callable[[str], no.Node]), to_instance=par.parse_stmt)
-        # binder.bind(els.ElementProcessingDriver.Config(step_shuffle=True))
 
         injector = inj.create_injector(binder)
 
@@ -85,28 +96,35 @@ def test_inject(harness: har.Harness):
             ])
             connectors = drv[ctrs.ConnectorSet]
 
-        elements = check.isinstance(elements, els.ElementSet)
-        selements = serde.serialize(list(elements))
-        delements = els.ElementSet.of(serde.deserialize(selements, ta.Sequence[els.Element]))
-        assert list(delements) == list(elements)
 
-        print(yaml.dump(selements))
+def test_inject(harness: har.Harness, exit_stack):
+    exit_stack.enter_context(oos.tmp_chdir(os.path.dirname(__file__)))
 
-        nsa = elements.analyze(tars.NamespaceAnalysis)  # noqa
+    elements = check.isinstance(elements, els.ElementSet)
+    selements = serde.serialize(list(elements))
+    delements = els.ElementSet.of(serde.deserialize(selements, ta.Sequence[els.Element]))
+    assert list(delements) == list(elements)
 
-        conns = es.enter_context(contextlib.closing(ctrs.ConnectionSet(connectors)))
-        execution_injector = injector[inj.Key(inj.Injector, 'execution')]
-        with ops.inject.new_execution_scope(execution_injector, conns):
-            matrs_by_mat_id = unique_dict((matr.target.id, matr) for matr in elements.get_type_set(pln.Materializer))
-            deps = {mat_id: {d.id for d in matr.srcs} for mat_id, matr in matrs_by_mat_id.items()}
-            oed: ops.OpExecutionDriver = execution_injector[ops.OpExecutionDriver]
-            for mat_id in itertools.chain.from_iterable(ocol.toposort(deps)):
-                matr = matrs_by_mat_id[mat_id]
-                oed.execute(matr.op)
+    print(yaml.dump(selements))
 
-        with harness[DbManager].pg_engine.connect() as pg_conn:
-            print(list(pg_conn.execute('select * from a')))
-            print(list(pg_conn.execute('select * from b')))
-            print(list(pg_conn.execute('select * from c')))
-            print(list(pg_conn.execute('select * from d')))
-            print(list(pg_conn.execute('select * from nums')))
+    nsa = elements.analyze(tars.NamespaceAnalysis)  # noqa
+
+    conns = exit_stack.enter_context(contextlib.closing(ctrs.ConnectionSet(connectors)))
+    execution_injector = injector[inj.Key(inj.Injector, 'execution')]
+    with ops.inject.new_execution_scope(execution_injector, conns):
+        matrs_by_mat_id = unique_dict((matr.target.id, matr) for matr in elements.get_type_set(pln.Materializer))
+        deps = {mat_id: {d.id for d in matr.srcs} for mat_id, matr in matrs_by_mat_id.items()}
+        oed: ops.OpExecutionDriver = execution_injector[ops.OpExecutionDriver]
+        for mat_id in itertools.chain.from_iterable(ocol.toposort(deps)):
+            matr = matrs_by_mat_id[mat_id]
+            oed.execute(matr.op)
+
+    with harness[DbManager].pg_engine.connect() as pg_conn:
+        print(list(pg_conn.execute('select * from a')))
+        print(list(pg_conn.execute('select * from b')))
+        print(list(pg_conn.execute('select * from c')))
+        print(list(pg_conn.execute('select * from d')))
+        print(list(pg_conn.execute('select * from nums')))
+
+
+# class BaseTestFuck(unittest.TestCase):
